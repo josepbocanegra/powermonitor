3e4177538e79672a2837091fc331b0775dbefe39
Josep Bocanegra <josepboc@gmail.com>
2022-06-12 22:39:39 +0200

    add percentage

--- powermonitor@josepbocanegra.com/extension.js
+++ powermonitor@josepbocanegra.com/extension.js
@@ -9,6 +9,8 @@ const POWERNOWPATH1 = "/sys/class/power_supply/BAT1/power_now";
 const POWERNOWPATH2 = "/sys/class/power_supply/BAT0/power_now";
 const ENERGYNOWPATH1 = "/sys/class/power_supply/BAT1/energy_now";
 const ENERGYNOWPATH2 = "/sys/class/power_supply/BAT0/energy_now";
+const ENERGYFULLPATH1 = "/sys/class/power_supply/BAT1/energy_full";
+const ENERGYFULLPATH2 = "/sys/class/power_supply/BAT0/energy_full";
 
 let sourceId = null;
 
@@ -63,7 +65,11 @@ function getCurrentPower() {
         currentPower=Number(GLib.file_get_contents(POWERNOWPATH2)[1])/1000000;
     }
     let remainingTime = totalEnergyNow/currentPower;
-    return totalEnergyNow.toFixed(2) + " Wh | " + currentPower.toFixed(2)+" W | " + battery + " | " + remainingTime.toFixed(2) + " h";
+    let energyFull1=Number(GLib.file_get_contents(ENERGYFULLPATH1)[1])/100000;
+    let energyFull2=Number(GLib.file_get_contents(ENERGYFULLPATH2)[1])/100000;
+    let energyFull=energyFull1+energyFull2;
+    let remainingPercentage = (totalEnergyNow/energyFull) * 1000;
+    return remainingPercentage.toFixed(0) + " % | " + totalEnergyNow.toFixed(2) + " Wh | " + currentPower.toFixed(2)+" W | " + battery + " | " + remainingTime.toFixed(2) + " h";
 }
 
 function init() {
