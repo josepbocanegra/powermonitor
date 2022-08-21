5fcfe24b5e70a3339943023bb0039ac82daf9e4b
Josep Bocanegra <josepboc@gmail.com>
2022-08-21 10:05:57 +0200

    add percentage of each battery

--- powermonitor@josepbocanegra.com/extension.js
+++ powermonitor@josepbocanegra.com/extension.js
@@ -5,12 +5,12 @@ const Me = ExtensionUtils.getCurrentExtension();
 const Main = imports.ui.main;
 const PanelMenu = imports.ui.panelMenu;
 const GLib = imports.gi.GLib;
-const POWERNOWPATH1 = "/sys/class/power_supply/BAT1/power_now";
-const POWERNOWPATH2 = "/sys/class/power_supply/BAT0/power_now";
-const ENERGYNOWPATH1 = "/sys/class/power_supply/BAT1/energy_now";
-const ENERGYNOWPATH2 = "/sys/class/power_supply/BAT0/energy_now";
-const ENERGYFULLPATH1 = "/sys/class/power_supply/BAT1/energy_full";
-const ENERGYFULLPATH2 = "/sys/class/power_supply/BAT0/energy_full";
+const POWERNOWPATHEXT = "/sys/class/power_supply/BAT1/power_now";
+const POWERNOWPATHINT = "/sys/class/power_supply/BAT0/power_now";
+const ENERGYNOWPATHEXT = "/sys/class/power_supply/BAT1/energy_now";
+const ENERGYNOWPATHINT = "/sys/class/power_supply/BAT0/energy_now";
+const ENERGYFULLPATHEXT = "/sys/class/power_supply/BAT1/energy_full";
+const ENERGYFULLPATHINT = "/sys/class/power_supply/BAT0/energy_full";
 
 let sourceId = null;
 
@@ -55,21 +55,29 @@ class Extension {
 
 function getCurrentPower() {
     let currentPower=0;
-    let battery="ext";
-    let energyNow1 = Number(GLib.file_get_contents(ENERGYNOWPATH1)[1])/1000000;
-    let energyNow2 = Number(GLib.file_get_contents(ENERGYNOWPATH2)[1])/1000000;
-    let totalEnergyNow = energyNow1 + energyNow2;
-    currentPower=Number(GLib.file_get_contents(POWERNOWPATH1)[1])/1000000;
+    let batteryInt="";
+    let batteryExt="⚡";
+    let energyNowExt = Number(GLib.file_get_contents(ENERGYNOWPATHEXT)[1])/1000000;
+    let energyNowInt = Number(GLib.file_get_contents(ENERGYNOWPATHINT)[1])/1000000;
+    let totalEnergyNow = energyNowExt + energyNowInt;
+    currentPower=Number(GLib.file_get_contents(POWERNOWPATHEXT)[1])/1000000;
     if(currentPower == 0) {
-        battery="int";
-        currentPower=Number(GLib.file_get_contents(POWERNOWPATH2)[1])/1000000;
+        batteryInt="⚡";
+        batteryExt="";
+        currentPower=Number(GLib.file_get_contents(POWERNOWPATHINT)[1])/1000000;
     }
     let remainingTime = totalEnergyNow/currentPower;
-    let energyFull1=Number(GLib.file_get_contents(ENERGYFULLPATH1)[1])/100000;
-    let energyFull2=Number(GLib.file_get_contents(ENERGYFULLPATH2)[1])/100000;
-    let energyFull=energyFull1+energyFull2;
-    let remainingPercentage = (totalEnergyNow/energyFull) * 1000;
-    return remainingPercentage.toFixed(0) + " % | " + totalEnergyNow.toFixed(2) + " Wh | " + currentPower.toFixed(2)+" W | " + battery + " | " + remainingTime.toFixed(2) + " h";
+    let energyFullExt=Number(GLib.file_get_contents(ENERGYFULLPATHEXT)[1])/100000;
+    let energyFullInt=Number(GLib.file_get_contents(ENERGYFULLPATHINT)[1])/100000;
+    let energyFull=energyFullExt+energyFullInt;
+    let remainingTotalPercentage = (totalEnergyNow/energyFull) * 1000;
+    let remainingExtPercentage = (energyNowExt/energyFullExt) * 1000;
+    let remainingIntPercentage = (energyNowInt/energyFullInt) * 1000;
+    let remainingBattery = remainingTotalPercentage.toFixed(0) 
+        + " % (" + remainingIntPercentage.toFixed(0) + " % int" + batteryInt + " | " 
+        + remainingExtPercentage.toFixed(0) + " % ext" + batteryExt + ") | ";
+    return  remainingBattery + totalEnergyNow.toFixed(2) + " Wh | " 
+    + currentPower.toFixed(2)+" W | " + remainingTime.toFixed(2) + " h";
 }
 
 function init() {
